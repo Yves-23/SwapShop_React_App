52bb9893b2dc3d0406ac377a3d73bf68c36f3f16
Yves-23 <habinezan.dyves@gmail.com>
2024-11-25 03:12:35 +0200

    I worked on SubHeader & Slider

--- SwapShop/src/App.jsx
+++ SwapShop/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import ImageSlider from "./Components/ImageSlider/ImageSlider";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <ImageSlider />
     </div>
   );
 };
